b223f3c9de59a19d0d04d09802f04f768eb4e912
Park <rsital@icloud.com>
2018-03-05 20:00:01 +0100

    Initial Commit

---
+++ RSR-3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSR-3
+//
+//  Created by Parvin Sital on 05/03/2018.
+//  Copyright © 2018 Parvin Sital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RSR-3Tests/RSR_3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  RSR_3Tests.swift
+//  RSR-3Tests
+//
+//  Created by Parvin Sital on 05/03/2018.
+//  Copyright © 2018 Parvin Sital. All rights reserved.
+//
+
+import XCTest
+@testable import RSR_3
+
+class RSR_3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
