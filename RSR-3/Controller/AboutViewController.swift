5a5ee44a666e581c35ba1c0b8b98bf77e45f1c55
Park <rsital@icloud.com>
2018-03-08 13:43:18 +0100

    Properly fixing all UI elements with size classes.

---
+++ RSR-3/Controller/AboutViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AboutViewController.swift
+//  RSR-3
+//
+//  Created by Parvin Sital on 08/03/2018.
+//  Copyright © 2018 Parvin Sital. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            self.scrollView.isScrollEnabled = false
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
